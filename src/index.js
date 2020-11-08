af817df15107b930064004d808acdd98cff99934
ehddnr1294 <ehddnr1294@gmail.com>
2020-11-09 00:33:51 +0900

    Holy symbol

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Page1 from './Page1';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Switch>
+    <Route exact path="/" component={App} /> 
+    <Route exact path="/page" component={Page1} />
+    </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+);  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
